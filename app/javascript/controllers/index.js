ead2de79c57857af96022c8fd5c3c5fef9bbbbc8
brymopo <bmoreno.dl@gmail.com>
2022-02-07 15:10:59 -0500

    add room-form stimulus controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,8 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
+import HelloController from "./hello_controller.js"
 application.register("hello", HelloController)
+
+import RoomFormController from "./room_form_controller.js"
+application.register("room-form", RoomFormController)
