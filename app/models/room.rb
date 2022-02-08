ed8f697f3cb239b56f99ba9bc44791152b7950d2
brymopo <bmoreno.dl@gmail.com>
2022-02-08 17:38:52 -0500

    add dependent destroy to room model

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,5 @@
 class Room < ApplicationRecord
   broadcasts_to ->(_){ "rooms" }, inserts_by: :append, target: "rooms"
   validates :name, presence: true
-  has_many :messages
+  has_many :messages, dependent: :destroy
 end
