449a5b2dcff63404cadc4acc2ecf1f65f53b8739
brymopo <bmoreno.dl@gmail.com>
2022-02-08 16:23:14 -0500

    add create method to messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,26 @@
 class MessagesController < ApplicationController
-  before_action :set_room
+  before_action :set_room, only: %i[new create]
 
   def new
     @message = @room.messages.new
+  end
+
+  def create
+    @message = @room.messages.create(message_params)
+
+    if @message.save
+      @message = @room.messages.new
+    end
+    render :new
   end
 
   private
 
     def set_room
       @room = Room.find(params[:room_id])
+    end
+
+    def message_params
+      params.require(:message).permit(:content, :author)
     end
 end
