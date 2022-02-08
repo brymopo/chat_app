458750bff2f8904663d1685821091a806d1197f0
brymopo <bmoreno.dl@gmail.com>
2022-02-08 10:01:31 -0500

    add messages routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "rooms#index"
-  resources :rooms, except: %i[destroy]
+  resources :rooms, except: %i[destroy] do
+    resources :messages, except: %i[destroy]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
