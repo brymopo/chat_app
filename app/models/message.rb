4e8b6da9dc11e76313772584a85e52ac7ffa9b60
brymopo <bmoreno.dl@gmail.com>
2022-02-08 16:23:37 -0500

    add broadcasts_to to message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ApplicationRecord
   belongs_to :room
   validates :author, :content, presence: true
+  broadcasts_to ->(_){ "room_messages" }, inserts_by: :append, target: "room_messages"
 end
