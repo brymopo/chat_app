35a959aefe1f87de37e56d79975127e886dc4532
brymopo <bmoreno.dl@gmail.com>
2022-02-08 17:39:46 -0500

    add edited? method to message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,8 @@ class Message < ApplicationRecord
   belongs_to :room
   validates :author, :content, presence: true
   broadcasts_to ->(_){ "room_messages" }, inserts_by: :append, target: "room_messages"
+
+  def edited?
+    created_at != updated_at
+  end
 end
