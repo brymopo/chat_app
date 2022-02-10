cc4bdf7968100004f48cf2d0c22319a38e664142
brymopo <bmoreno.dl@gmail.com>
2022-02-10 18:11:13 -0500

    refactor chat room scopes

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :room
+  after_create_commit { broadcast_append_to(room, inserts_by: "append", target: "room_#{room_id}_messages") }
   validates :author, :content, presence: true
   broadcasts
 
