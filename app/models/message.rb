ce948291b042505ebaba9184dac6b33517dd9d8a
brymopo <bmoreno.dl@gmail.com>
2022-02-10 15:55:22 -0500

    refactor message broadcasts to better scope it

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :room
   validates :author, :content, presence: true
-  broadcasts_to ->(_){ "room_messages" }, inserts_by: :append, target: "room_messages"
+  broadcasts
 
   def edited?
     created_at != updated_at
