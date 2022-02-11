27b8ebcd53cb9f65d0f7e4e44a81a05e258d54f3
brymopo <bmoreno.dl@gmail.com>
2022-02-11 14:43:13 -0500

    add modal stimulus controller

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -2,6 +2,39 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="modal"
 export default class extends Controller {
+  static targets = [ "modalBody" ]
+
   connect() {
+  }
+
+  showModal(){
+    if(this.isModalClosed()){
+      this.modalBodyTarget.classList.remove("hidden");
+    }
+  }
+
+  hideModal(){
+    if(this.isModalOpen) {
+      this.modalBodyTarget.classList.add("hidden");
+    }
+    this.resetModal();
+  }
+
+  isModalClosed(){
+    return this.modalBodyTarget.classList.contains("hidden")
+  }
+
+  isModalOpen(){
+    return !this.isModalClosed();
+  }
+
+  resetModalFrame(){
+    const modalFrame = document.getElementById("room-modal");
+    if(modalFrame){
+      modalFrame.setAttribute("src", undefined);
+      while(modalFrame.firstChild){
+        modalFrame.firstChild.remove();
+      }
+    }    
   }
 }
