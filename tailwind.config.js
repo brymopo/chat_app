ff90433ad2c6eb9d3753d6a05c8ac6ac9fcc152c
brymopo <bmoreno.dl@gmail.com>
2022-02-11 16:07:44 -0500

    add styles to user new view

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
-    './app/javascript/**/*.js'
+    './app/javascript/**/*.js',
+    './app/assets/stylesheets/**/*.css'
   ]
 }
