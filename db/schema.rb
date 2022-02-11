8e7f828f48ca46d05b559ed5380c343095f28199
brymopo <bmoreno.dl@gmail.com>
2022-02-11 00:10:13 -0500

    add user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_07_215157) do
+ActiveRecord::Schema.define(version: 2022_02_11_050908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,15 @@ ActiveRecord::Schema.define(version: 2022_02_07_215157) do
     t.integer "id7"
     t.integer "id8"
     t.integer "id9"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "messages", "rooms"
