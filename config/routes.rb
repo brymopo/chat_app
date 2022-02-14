4ad9c5b20ace6f4cf12bbd36b15c7c7230bf78a1
brymopo <bmoreno.dl@gmail.com>
2022-02-14 09:11:10 -0500

    add devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "users/registrations", sessions: "users/sessions" }
   root "rooms#index"
   resources :rooms, except: %i[destroy] do
     resources :messages, except: %i[destroy]
