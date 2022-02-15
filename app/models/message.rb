54ee91c03610923bbdc56f6788c01eb7b7ff9436
brymopo <bmoreno.dl@gmail.com>
2022-02-15 10:51:30 -0500

    break down broadcasts_to to make current_user available to turbo stream partials

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,11 @@
 class Message < ApplicationRecord
   belongs_to :room
-  validates :author, :content, presence: true
-  broadcasts_to ->(message){ message.room }, inserts_by: "append", target: "messages"
+  validates :content, presence: true
+  validates :author, presence: { message: "must be logged in to send messages" }
+  #broadcasts_to ->(message){ message.room }, inserts_by: "append", target: "messages"
+  after_create_commit ->(message){ broadcast_action_later_to message.room, action: "append", target: "messages", locals: { user: Current.user } }
+  after_update_commit ->(message){ broadcast_replace_later_to message.room, target: self, locals: { user: Current.user } }
+  after_destroy_commit ->(message){ broadcast_remove_to message.room }
 
   def edited?
     created_at != updated_at
