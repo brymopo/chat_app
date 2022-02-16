8161849eace77850173c0e8c0404573941512eb1
brymopo <bmoreno.dl@gmail.com>
2022-02-16 18:04:40 -0500

    add users to message model and remove author validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :room
+  belongs_to :user
   validates :content, presence: true
-  validates :author, presence: { message: "must be logged in to send messages" }
   broadcasts_to ->(message){ message.room }, inserts_by: "append", target: "messages"
 # after_create_commit ->(message){ broadcast_action_later_to message.room, partial: "rooms/room-detail", action: "append", target: "messages", locals: { user: Current.user } }
 # after_update_commit ->(message){ broadcast_replace_later_to message.room, target: self, locals: { user: Current.user } }
