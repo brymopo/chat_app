8811367ec20c9401aadc34ddadf876878dddf45e
brymopo <bmoreno.dl@gmail.com>
2022-02-16 18:08:33 -0500

    remove set_current_user method from messages controller and modify update action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,13 @@
 class MessagesController < ApplicationController  
   before_action :set_room, only: %i[new edit create]
   before_action :set_message, only: %i[edit update show]
-  before_action :set_current_user, only: %i[create update]
   
   def new
     @message = @room.messages.new
   end
 
   def create
-    author = current_user.present? ? current_user.username : nil
-    @message = @room.messages.create(message_params.merge(author: author))
+    @message = @room.messages.create(message_params.merge(user_id: current_user.id))
 
     if @message.save
       @message = @room.messages.new     
@@ -41,10 +39,6 @@ class MessagesController < ApplicationController
     end
 
     def message_params
-      params.require(:message).permit(:content, :author)
-    end
-
-    def set_current_user
-      Current.user = current_user
+      params.require(:message).permit(:content)
     end
 end
