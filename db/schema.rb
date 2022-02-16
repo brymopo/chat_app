6faa31c4c50460f78b5a6374049e9f30d64cb6f4
brymopo <bmoreno.dl@gmail.com>
2022-02-16 17:45:14 -0500

    update schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_11_212433) do
+ActiveRecord::Schema.define(version: 2022_02_16_224243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "messages", force: :cascade do |t|
-    t.string "author"
     t.text "content"
     t.bigint "room_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
     t.index ["room_id"], name: "index_messages_on_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "rooms", force: :cascade do |t|
@@ -65,4 +66,5 @@ ActiveRecord::Schema.define(version: 2022_02_11_212433) do
   end
 
   add_foreign_key "messages", "rooms"
+  add_foreign_key "messages", "users"
 end
